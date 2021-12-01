77aed9d29ce1ab3ce6597f658e0e898adf1adc47
Inixio <inixio.amillano@inixio.dev>
2021-12-01 22:03:20 +0100

    ADD HTTPS support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(process.env.PORT || 3000);
+const crPath = '/etc/letsencrypt/live/testapi.leiva.io/fullchain.pem';
+const pkPath = '/etc/letsencrypt/live/testapi.leiva.io/privkey.pem';
+const options: any = {};
+
+import * as fs from 'fs';
+
+// validamos si los archivos existen
+if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
+  // cargamos los archivos sobre las options
+  options.httpsOptions = {
+    cert: fs.readFileSync(crPath),
+    key: fs.readFileSync(pkPath)
+  }
 }
+
+async function bootstrap() {
+  // le pasamos las options al NestFactory
+  const app = await NestFactory.create(AppModule, options);
+  await app.listen(3000);
+}
+bootstrap();
 bootstrap();
