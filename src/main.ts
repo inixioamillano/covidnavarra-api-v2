b246af825e215641cd2984c6a77c96ace4f15f7b
Inixio <inixio.amillano@inixio.dev>
2021-12-02 14:25:39 +0100

    UPDATE path

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const crPath = '/etc/letsencrypt/live/inixio.dev/fullchain.pem';
-const pkPath = '/etc/letsencrypt/live/inixio.dev/privkey.pem';
+const crPath = '/etc/letsencrypt/live/covidnavarraapi.inixio.dev/fullchain.pem';
+const pkPath = '/etc/letsencrypt/live/covidnavarraapi.inixio.dev/privkey.pem';
 const options: any = {};
 
 import * as fs from 'fs';
 
-// validamos si los archivos existen
 if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
-  // cargamos los archivos sobre las options
   options.httpsOptions = {
     cert: fs.readFileSync(crPath),
     key: fs.readFileSync(pkPath)
